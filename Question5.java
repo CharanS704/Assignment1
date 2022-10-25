90662668051492876585810e2729ebdc1e24b9ee
CharanS704 <115578406+CharanS704@users.noreply.github.com>
2022-10-25 16:53:28 +0530

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -5,7 +5,8 @@ public class Question5 {
 		int n = 14;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (i == 0 || j == 0 || i == (n - 1) || i + j <= ((n - 1) / 2) || i - j >= ((n - 1) / 2))//Logic for printing the shape {
+				if (i == 0 || j == 0 || i == (n - 1) || i + j <= ((n - 1) / 2) || i - j >= ((n - 1) / 2))//Logic for printing the shape 
+				{
 					System.out.print("*");
 				} else {
 					System.out.print(" ");
